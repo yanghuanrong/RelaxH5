cf9a3368d95f0bf90222ad3552e5ae9693ae3706
yanghuanrong <1176989241@qq.com>
2020-07-02 13:34:36 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <button type="button" @click="download">button</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import EJS from "ejs";
+import FileSaver from "file-saver";
+
+const VUE_NAME = 'This#is#fileName'
+
+const outExportFileByStr = (fileName,str) => {
+    str = str.replace(VUE_NAME,fileName.replace('.vue',''))
+    let blob = new Blob([str], {type: "text/plain;charset=utf-8"});
+    FileSaver.saveAs(blob, fileName)
+}
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  methods: {
+    download() {
+      var ejs = require("ejs");
+      var people = ["geddy", "neil", "alex"];
+      const temp = '<div><%= people.join(", ") %></div>';
+      var html = ejs.render(temp, { people: people });
+      console.log(html);
+
+      outExportFileByStr('hha.vue', html)
+      
+    }
   }
-}
+};
 </script>
