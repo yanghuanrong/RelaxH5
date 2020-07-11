9f8de2583652f160f5aa5bf44f7a9d41fed56f96
yanghuanrong <1176989241@qq.com>
2020-07-11 10:36:23 +0800

    update

--- client/views/editor.vue
+++ client/views/editor.vue
@@ -40,17 +40,37 @@
 
     <div class="page-wrapper">
       <div class="editor-toolbar__left">
-        <a-tabs default-active-key="1" tab-position="left">
-          <a-tab-pane key="1" tab="Tab 1">Content of Tab 1</a-tab-pane>
-          <a-tab-pane key="2" tab="Tab 2">Content of Tab 2</a-tab-pane>
-          <a-tab-pane key="3" tab="Tab 3">Content of Tab 3</a-tab-pane>
-        </a-tabs>
+        <draggable
+          :list="list"
+          :options="{sort:false}"
+          :group="{ name: 'layouts', pull: 'clone', put: false}"
+        >
+          <div
+            style="width: 100px; height: 100px; background: #F00"
+            v-for="(item, i) in list"
+            :key="i + '1'"
+          >
+            {{item.name}}
+          </div>
+        </draggable>
       </div>
 
       <div class="editor-toolbar__center">
-        <div class="edit-main"></div>
+        <div class="edit-main">
+           <draggable group="layouts" :list="listb"
+                       :options="{
+                   }"
+            >
+                <template v-for="(item, i) in listb" >
+                  <div>{{item.name}}</div>
+                    <!-- <component :key="index" :is="item.componentName"
+                               :dataIIndex="index + ''"
+                               v-bind="item"
+                    ></component> -->
+                </template>
+            </draggable>
+        </div>
       </div>
-
 
       <div class="editor-toolbar__right"></div>
     </div>
@@ -58,7 +78,17 @@
 </template>
 
 <script>
+import draggable from "vuedraggable";
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      listb: [],
+      list: [{id:1,name:'嘿嘿'}, {id:2,name:'方法'}, {id:3,name:'嗯嗯'}, {id:4,name:'刚刚'}]
+    };
+  }
 };
 </script>
