d2782b24e469e5c58a81db78a62844d6398896e7
yanghuanrong <1176989241@qq.com>
2020-09-03 23:59:58 +0800

    update

--- client/views/editProject/rightComponent/component/edit-swiper.vue
+++ client/views/editProject/rightComponent/component/edit-swiper.vue
@@ -5,9 +5,13 @@
       <dl class="img-array" v-for="(item, i) in component.componentAttrs.images" :key="i">
         <dt>
           <van-image width="96" height="96" :src="item" />
-        </dt> 
+        </dt>
         <dd>
-          <Input type="textarea" :rows="4" :value="item" />
+          <Input type="textarea" :rows="4" v-model="images[i]" @on-blur="change(i, images[i])" />
+          <ButtonGroup shape="circle" class="operation" size="small">
+            <Button icon="md-add" @click="add(item)"></Button>
+            <Button icon="md-remove" @click="remvoe(i)"></Button>
+          </ButtonGroup>
         </dd>
       </dl>
     </TabPane>
@@ -24,6 +28,7 @@ export default {
   data() {
     return {
       component: {},
+      images: {},
       componentStyle: {},
       styleTextarea: "",
     };
@@ -37,11 +42,30 @@ export default {
     this.$set(this.componentStyle, "width", element.offsetWidth);
     this.$set(this.componentStyle, "height", element.offsetHeight);
 
-    console.log(this.component.componentAttrs.images)
+    this.synchronous();
+    console.log(this.component.componentAttrs.images);
   },
   methods: {
     saveComponent() {
       this.$store.commit("saveComponent", this.component);
+      this.synchronous();
+    },
+    synchronous() {
+      this.component.componentAttrs.images.forEach((item, i) => {
+        this.images[i] = item;
+      });
+    },
+    remvoe(i) {
+      this.component.componentAttrs.images.splice(i, 1);
+      this.saveComponent();
+    },
+    add(src) {
+      this.component.componentAttrs.images.push(src);
+      this.saveComponent();
+    },
+    change(i, value) {
+      this.component.componentAttrs.images[i] = value
+      this.saveComponent();
     },
     onChange(value) {
       const oldStyle = this.componentStyle;
@@ -62,12 +86,21 @@ export default {
 <style lang="less" scope="this api replaced by slot-scope in 2.5.0+">
 .img-array {
   display: flex;
-  dd{
+  margin-bottom: 15px;
+
+  dt {
+    width: 100px;
+  }
+  dd {
     flex: 1;
     padding-left: 10px;
-  }
-  dt{
-    width: 100px;
+    position: relative;
+
+    .operation {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+    }
   }
 }
 </style>
