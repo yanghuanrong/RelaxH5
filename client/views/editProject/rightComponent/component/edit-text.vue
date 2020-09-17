7f439a9ff93e050a221025a2172222ea277e2fd9
yanghuanrong <1176989241@qq.com>
2020-09-17 12:59:08 +0800

    fontsize

--- client/views/editProject/rightComponent/component/edit-text.vue
+++ client/views/editProject/rightComponent/component/edit-text.vue
@@ -1,8 +1,36 @@
 <template>
   <div>
-    <p>
-      <Input addon-before="文本值" v-model="component.componentText" @blur="saveComponent" />
-    </p>
+    <Tabs>
+      <TabPane label="样式">
+        <Form :label-width="80">
+          <FormItem label="字号">
+            <Dropdown
+              placement="bottom-start"
+              @on-click="selectFontSize"
+              trigger="custom"
+              :visible="fontSizeDrap"
+            >
+              <Input
+                style="width:100px"
+                suffix="ios-arrow-down"
+                v-model="fontSizeValue"
+                @on-blur="fontSizeDrap = false"
+                @on-focus="fontSizeDrap = true"
+              />
+              <DropdownMenu slot="list">
+                <DropdownItem :name="item" v-for="item in fontSizeList" :key="item">{{ item+'px' }}</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+
+            <ButtonGroup style="margin-left: 20px">
+              <Button>A+</Button>
+              <Button>A-</Button>
+            </ButtonGroup>
+
+          </FormItem>
+        </Form>
+      </TabPane>
+    </Tabs>
   </div>
 </template>
 
@@ -10,11 +38,14 @@
 export default {
   name: "editText",
   props: {
-    drawerComponent: Object
+    drawerComponent: Object,
   },
   data() {
     return {
-      component: {}
+      fontSizeList: [12, 13, 14, 16, 18, 20, 24, 32, 48, 64, 96],
+      fontSizeDrap: false,
+      fontSizeValue: 15,
+      component: {},
     };
   },
   created() {
@@ -23,7 +54,11 @@ export default {
   methods: {
     saveComponent() {
       this.$store.commit("saveComponent", this.component);
-    }
-  }
+    },
+    selectFontSize(val) {
+      this.fontSizeValue = val;
+      console.log(val);
+    },
+  },
 };
 </script>
