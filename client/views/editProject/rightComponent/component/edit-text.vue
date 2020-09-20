14e3faa7241eb93cc0dbd2ec56b8390c11e94251
yanghuanrong <1176989241@qq.com>
2020-09-21 00:04:02 +0800

    对齐与加粗

--- client/views/editProject/rightComponent/component/edit-text.vue
+++ client/views/editProject/rightComponent/component/edit-text.vue
@@ -67,6 +67,34 @@
             ></Slider>
           </FormItem>
         </Form>
+
+        <div class="align">
+          <Tooltip content="居左" placement="top">
+            <span class="x-icon x-icon-align-left" :class="{active: componentStyle.textAlign === 'left'}" @click="setTextAlign('left')"></span>
+          </Tooltip>
+          <Tooltip content="居中" placement="top">
+            <span class="x-icon x-icon-align-center" :class="{active: componentStyle.textAlign === 'center'}" @click="setTextAlign('center')"></span>
+          </Tooltip>
+          <Tooltip content="居右" placement="top">
+            <span class="x-icon x-icon-align-right" :class="{active: componentStyle.textAlign === 'right'}" @click="setTextAlign('right')"></span>
+          </Tooltip>
+          <Tooltip content="两端对齐" placement="top">
+            <span class="x-icon x-icon-align-justify" :class="{active: componentStyle.textAlign === 'justify'}" @click="setTextAlign('justify')"></span>
+          </Tooltip>
+          <i></i>
+          <Tooltip content="加粗" placement="top">
+            <span class="x-icon x-icon-bold" :class="{active: componentStyle.fontWeight === 'bold'}" @click="setFontWeighh"></span>
+          </Tooltip>
+          <Tooltip content="倾斜" placement="top">
+            <span class="x-icon x-icon-italic"></span>
+          </Tooltip>
+          <Tooltip content="下划线" placement="top">
+            <span class="x-icon x-icon-underline"></span>
+          </Tooltip>
+          <Tooltip content="清除" placement="top">
+            <span class="x-icon x-icon-trash"></span>
+          </Tooltip>
+        </div>
       </TabPane>
     </Tabs>
   </div>
@@ -82,8 +110,6 @@ export default {
     return {
       fontSizeList: [12, 13, 14, 16, 18, 20, 24, 32, 48, 64, 96],
       fontSizeDrap: false,
-      fontBackground: "",
-      fontTextColor: "",
       component: {},
       componentStyle: {
         fontSize: 14,
@@ -91,6 +117,8 @@ export default {
         letterSpacing: 0,
         color: "",
         background: "",
+        textAlign: "left",
+        fontWeight: "normal",
       },
     };
   },
@@ -98,24 +126,28 @@ export default {
     this.component = this.drawerComponent;
     const style = this.component.componentAttrs.style;
 
-    this.componentStyle.fontSize = style.fontSize ? parseInt(style.fontSize) : 14;
+    this.componentStyle.fontSize = style.fontSize
+      ? parseInt(style.fontSize)
+      : 14;
     this.componentStyle.lineHeight = style.lineHeight || 1.5;
-    this.componentStyle.letterSpacing = parseFloat(style.letterSpacing)  * 2 || 0
-    this.componentStyle.color = style.color || ''
-    this.componentStyle.background = style.background || ''
-
+    this.componentStyle.letterSpacing =
+      parseFloat(style.letterSpacing) * 2 || 0;
+    this.componentStyle.color = style.color || "";
+    this.componentStyle.background = style.background || "";
+    this.componentStyle.textAlign = style.textAlign || 'left'
+    this.componentStyle.fontWeight = style.fontWeight || 'normal'
   },
   watch: {
     // 字体大小
-    "componentStyle.fontSize": function(val){
-      this.setStyle('fontSize', val + 'px')
+    "componentStyle.fontSize": function (val) {
+      this.setStyle("fontSize", val + "px");
     },
-    // 
-    "componentStyle.background": function(val){
-      this.setStyle('background', val)
+    //
+    "componentStyle.background": function (val) {
+      this.setStyle("background", val);
     },
-    "componentStyle.color": function(val){
-      this.setStyle('color', val)
+    "componentStyle.color": function (val) {
+      this.setStyle("color", val);
     },
   },
   methods: {
@@ -139,16 +171,51 @@ export default {
     },
     // 文字行高
     setLineHeight(val) {
-      this.setStyle('lineHeight', val)
+      this.setStyle("lineHeight", val);
     },
     // 文字间距
     setLetterSpacing(val) {
-      this.setStyle('letterSpacing', val * 0.5 + "px")
+      this.setStyle("letterSpacing", val * 0.5 + "px");
     },
-    setStyle(key, val){
+    // 文字对齐
+    setTextAlign(val){
+      this.componentStyle.textAlign = val
+      this.setStyle("textAlign", val);
+    },
+    // 文字加粗
+    setFontWeighh(){
+      this.componentStyle.fontWeight = this.componentStyle.fontWeight === "bold" ? "normal" : "bold"
+      this.setStyle("fontWeight", this.componentStyle.fontWeight);
+    },
+    setStyle(key, val) {
       this.$set(this.component.componentAttrs.style, key, val);
       this.saveComponent();
     },
   },
 };
 </script>
+
+<style lang="less" scope="this api replaced by slot-scope in 2.5.0+">
+.align {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 10px;
+
+  i{
+    width: 1px;
+    height: 25px;
+    background: #e8eaec;
+  }
+
+  span {
+    cursor: pointer;
+    padding: 5px;
+
+    &.active{
+      background: #57a3f3;
+      color: #FFF;
+      border-radius: 3px;
+    }
+  }
+}
+</style>
