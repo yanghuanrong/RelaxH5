1d88fa4e13c1fc2fd166a3c7670c0afd18b1c181
yanghuanrong <1176989241@qq.com>
2020-09-23 12:38:13 +0800

    project

--- client/views/editProject/leftComponent/index.vue
+++ client/views/editProject/leftComponent/index.vue
@@ -1,7 +1,93 @@
 <template>
   <div class="editor-toolbar__file">
     <div class="left-layout-wrap">
-      
+       <Tree :data="data5" :render="renderContent" class="project-tree" @on-select-change="test"></Tree>
     </div>
   </div>
 </template>
+
+<script>
+import project from './project'
+console.log(project)
+ export default {
+        data () {
+            return {
+                data5: [
+                    {
+                        title: 'RelaxH5',
+                        icon: 'x-icon x-icon-folder',
+                        type: 'folder',
+                        expand: true,
+                        render: (h, { root, node, data }) => {
+                            return <div class="file-row">
+                              <div class="file-name">
+                                <span class={data.icon}></span> {data.title}
+                              </div>
+                              <div class="file-button">
+                                { data.type === 'folder' 
+                                ?
+                                <span class="x-icon x-icon-plus" onClick={() => {this.append(data)}}></span>
+                                : 
+                                <span class="x-icon x-icon-remove" onClick={() => { alert(2) }}></span> 
+                                }
+                                <span class="x-icon x-icon-download"></span>
+                              </div>
+                            </div>
+                        },
+                        children: project
+                    }
+                ],
+            }
+        },
+        methods: {
+            renderContent (h, { root, node, data }) {
+                return <div class="file-row">
+                  <div class="file-name">
+                    <span class={data.icon}></span> {data.title}
+                  </div>
+                  <div class="file-button">
+                    { data.type === 'folder' 
+                    ?
+                    <span class="x-icon x-icon-plus" onClick={(e) => {e.stopPropagation(); this.append(data)}}></span>
+                    : 
+                    null
+                    }
+                    <span class="x-icon x-icon-download"></span>
+                    <span class="x-icon x-icon-trash-2" onClick={(e) => {e.stopPropagation(); this.remove(root, node, data) }}></span>
+                  </div>
+                </div>
+            },
+            test(data){
+              console.log(data)
+            },
+            append (data) {
+                const children = data.children || [];
+                children.push({
+                              title: 'About.vue',
+                              icon: 'x-icon-vue',
+                              type: 'file',
+                              expand: true
+                          },);
+                this.$set(data, 'children', children);
+            },
+            remove (root, node, data) {
+                console.log(data)
+
+                this.$Modal.confirm({
+                    title: `你确定删除 <span style="color:#F00">${data.title}</span> 吗?`,
+                    content: `<p>删除${data.title}后不可恢复，请谨慎处理。</p>`,
+                    onOk: () => {
+                        const parentKey = root.find(el => el === node).parent;
+                        const parent = root.find(el => el.nodeKey === parentKey).node;
+                        const index = parent.children.indexOf(data);
+                        parent.children.splice(index, 1);
+
+                        this.$Message.success('删除成功');
+                    },
+                });
+
+                
+            }
+        }
+    }
+</script>
---
+++ client/views/editProject/leftComponent/project.js
@@ -0,0 +1,101 @@
+export default [{
+  title: 'public',
+  icon: 'x-icon x-icon-folder',
+  type: 'folder',
+  expand: true,
+  children: [
+    {
+      title: 'favicon.ico',
+      icon: 'x-icon-vue',
+      type: 'ico',
+      expand: true
+    },
+    {
+      title: 'index.html',
+      icon: 'ivu-icon ivu-icon-logo-html5',
+      type: 'file',
+      expand: true
+    },
+  ]
+}, 
+{
+  title: 'src',
+  icon: 'x-icon x-icon-folder',
+  type: 'folder',
+  expand: true,
+  children: [
+    {
+      title: 'assets',
+      icon: 'x-icon x-icon-folder',
+      type: 'folder',
+      expand: true
+    },
+    {
+      title: 'router',
+      icon: 'x-icon x-icon-folder',
+      type: 'folder',
+      children: [
+        {
+          title: 'index.js',
+          icon: 'ivu-icon ivu-icon-logo-javascript',
+          type: 'file',
+          expand: true
+        },
+      ],
+    },
+    {
+      title: 'store',
+      icon: 'x-icon x-icon-folder',
+      type: 'folder',
+      children: [
+        {
+          title: 'index.js',
+          icon: 'ivu-icon ivu-icon-logo-javascript',
+          type: 'file',
+          expand: true
+        },
+      ],
+    },
+    {
+      title: 'style',
+      icon: 'x-icon x-icon-folder',
+      type: 'folder',
+      children: [
+        {
+          title: 'global.less',
+          icon: 'ivu-icon ivu-icon-logo-css3',
+          type: 'file',
+          expand: true
+        },
+      ],
+    },
+    {
+      title: 'view',
+      icon: 'x-icon x-icon-folder',
+      type: 'folder',
+      expand: true,
+      children: [
+        {
+          title: 'index.vue',
+          icon: 'x-icon-vue',
+          type: 'vue',
+          expand: true
+        },
+      ],
+    },
+    {
+      title: 'App.vue',
+      icon: 'x-icon-vue',
+      type: 'vue',
+      expand: true
+    },
+    {
+      title: 'main.js',
+      icon: 'ivu-icon ivu-icon-logo-javascript',
+      type: 'file',
+      expand: true
+    },
+  ]
+}, 
+
+]
